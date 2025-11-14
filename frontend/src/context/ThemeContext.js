4091e72e50aabe0514466dc5d95502e86d57441e
Laysa <laysa.viana@outlook.com>
2025-11-13 22:56:15 -0300

    feat: add context para o Theme

---
+++ frontend/src/context/ThemeContext.js
@@ -0,0 +1,31 @@
+import React, { createContext, useContext, useMemo, useState } from 'react';
+import { ThemeProvider, CssBaseline, GlobalStyles } from '@mui/material';
+import { lightTheme, darkTheme } from '../theme';
+
+// Cria o contexto
+const ThemeModeContext = createContext();
+
+export const useThemeMode = () => useContext(ThemeModeContext);
+
+export const ThemeModeProvider = ({ children }) => {
+  const [mode, setMode] = useState('light');
+
+  const toggleTheme = () => {
+    setMode((prev) => (prev === 'light' ? 'dark' : 'light'));
+  };
+
+  const theme = useMemo(
+    () => (mode === 'light' ? lightTheme : darkTheme),
+    [mode]
+  );
+
+  return (
+    <ThemeModeContext.Provider value={{ mode, toggleTheme }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+
+        {children}
+      </ThemeProvider>
+    </ThemeModeContext.Provider>
+  );
+};
